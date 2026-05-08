7a1ec3f3a4a6c1ff83dbb0e29cb1f99449042248
DinhQuy <quyhd@gm-group.vn>
2026-05-08 17:42:57 +0700

    feat auth

---
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,19 @@
+import '../entities/auth_user.dart';
+
+abstract class AuthRepository {
+  Future<AuthUser?> getCurrentUser();
+
+  Future<AuthUser> login({
+    required String email,
+    required String password,
+  });
+
+  Future<AuthUser> loginWithGoogle();
+
+  Future<AuthUser> register({
+    required String email,
+    required String password,
+  });
+
+  Future<void> resetPassword(String email);
+}
