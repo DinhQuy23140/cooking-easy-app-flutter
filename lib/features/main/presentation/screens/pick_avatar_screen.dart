dc74c54cb6fccfb950248e726c3242ba2063c82f
DinhQuy <quyhd@gm-group.vn>
2026-05-11 17:54:26 +0700

    layout chooose avatar image

--- lib/features/main/presentation/screens/pick_avatar_screen.dart
+++ lib/features/main/presentation/screens/pick_avatar_screen.dart
@@ -25,24 +25,56 @@ class PickAvatarScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Stack(
-                children: [
-                  Container(
-                    width: 90,
-                    height: 90,
-                    decoration: BoxDecoration(
-                      color: Color(0x33FFFFFF),
-                      borderRadius: BorderRadius.circular(45),
-                      border: Border.all(
-                        color: Color(0x66FFFFFF),
-                        width: 2,
-                      )
-                    ),
-                    child: Icon(Icons.person_2_outlined, size: 24,)
+              const SizedBox(height: 56),
+              Center(
+                child: SizedBox(
+                  width: 90,
+                  height: 90,
+                  child: Stack(
+                    children: [
+                      Container(
+                        width: 90,
+                        height: 90,
+                        decoration: BoxDecoration(
+                          color: Color(0x33FFFFFF),
+                          borderRadius: BorderRadius.circular(45),
+                          border: Border.all(
+                            color: Color(0x66FFFFFF),
+                            width: 2,
+                          )
+                        ),
+                        child: Icon(Icons.person_2_outlined, size: 24,)
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Icon(Icons.camera_alt_outlined))
+                    ],
                   ),
-                  Icon(Icons.camera_alt_outlined)
-                ],
-              )
+                ),
+              ),
+              const SizedBox(height: 14,),
+              SizedBox(
+                width: double.infinity,
+                child: const Text('Pick your avatar',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0xFFFFFFFF),
+                    fontSize: 22,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: const Text("Choose an avatar or upload your own photo",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color(0x99FFFFFF),
+                  fontSize: 13,
+                  fontWeight: FontWeight.w400,
+                ),)),
+              const SizedBox(height: 28,),
             ],
           ),
         ),
